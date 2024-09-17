e9a1d986df2f9ece33a780d316bace98107114c6
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-09-17 19:38:31 +0500

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'https://spa-chocolate.ru' },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://spa-chocolate.ru',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
-  // app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT || 4000);
