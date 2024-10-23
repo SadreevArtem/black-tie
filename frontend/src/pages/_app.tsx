aadaec2792fd5acf068dea37a7965e742bbf8847
macbook <macbook@macbooks-MacBook-Pro-2.local>
2024-10-23 15:13:17 +0500

    add metrics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,27 +1,65 @@
 import "@/styles/globals.css";
-import { HydrationBoundary,QueryClientProvider,QueryClient } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode, useState } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = (Component as NextPageWithLayout).getLayout ?? ((page: ReactElement) => page)
-  const [queryClient] = useState(()=> new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  }))
+  const getLayout =
+    (Component as NextPageWithLayout).getLayout ??
+    ((page: ReactElement) => page);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return getLayout(
-    <QueryClientProvider client={queryClient}>
-       <HydrationBoundary state={pageProps.dehydratedState}>
-      <Component {...pageProps} />
-      </HydrationBoundary>
-    </QueryClientProvider>
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+       
+          ym(98705568, "init", {
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true,
+               webvisor:true
+          });
+              `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/98705568"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </HydrationBoundary>
+      </QueryClientProvider>
+    </>
   );
 }
